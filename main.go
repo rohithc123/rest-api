00c9072d760bc3d40f6e4ec4ff56cb575944fc6f
Rohith C <rohithc.eec.cseb@gmail.com>
2023-04-26 11:36:00 +0530

    Added JSON file to pass information from server to client side

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Article struct {
+	Title string `json:"Title"`
+	Desc string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request){
+
+	articles := Articles{
+			 Article{Title:"Test Title",Desc: "Test Description",Content: "Hello World"},
+	}
+  fmt.Println("Endpoint hit:All articles endpoint")
+  json.NewEncoder(w).Encode(articles)
+}
 
 
 func homePage(w http.ResponseWriter, r *http.Request){
@@ -13,7 +31,8 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 func handleRequests(){
    http.HandleFunc("/", homePage)
-   log.Fatal(http.ListenAndServe(":8881",nil))
+   http.HandleFunc("/articles",allArticles)
+   log.Fatal(http.ListenAndServe(":8080",nil))
 }
 
 func main(){
